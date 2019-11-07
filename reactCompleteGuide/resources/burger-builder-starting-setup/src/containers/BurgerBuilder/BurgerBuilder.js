e5c8f5d7338eb58ca41a39851612ce1083aa248e
Dan Fitz <dan@danfitz.com>
2019-11-07 14:38:56 -0600

    Added basic components for burger builder

---
+++ reactCompleteGuide/resources/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Auxiliary from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuilder extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Auxiliary>
+        <Burger />
+        <div>Build Controls</div>
+      </Auxiliary>
+    );
+  };
+};
+
+export default BurgerBuilder;
