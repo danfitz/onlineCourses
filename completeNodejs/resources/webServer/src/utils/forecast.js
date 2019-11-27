e82ebd59370dd57142fa2414f8e150ab9b7b5faa
Dan Fitz <dan@danfitz.com>
2019-11-26 23:21:44 -0500

    Refactored geocode and forecast to remove destructuring (destructuring the response object doesn't work if response is null, i.e., error thrown)

--- completeNodejs/resources/webServer/src/utils/forecast.js
+++ completeNodejs/resources/webServer/src/utils/forecast.js
@@ -1,18 +1,19 @@
 const request = require('request')
 const geocode = require('./geocode')
 
-const forecast = (data) => {
+const forecast = data => {
   const url = `https://api.darksky.net/forecast/83914e528bdb1b8fe11edc0dc05453af/${data.latitude},${data.longitude}?units=si`
   
   return new Promise((resolve, reject) => {
     const config = { url, json: true }
 
-    request(config, (error, { body }) => {
+    request(config, (error, response) => {
       if (error) {
         reject('Unable to connect to weather service')
-      } else if (body.error) {
-        reject('Unable to find location')
+      } else if (response.body.error) {
+        reject('Unable to find location from latitude and longitude')
       } else {
+        const body = response.body
         const newData = {
           ...data,
           forecast: `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degree celsius outside. There is ${body.currently.precipProbability}% chance of rain.`
--- completeNodejs/resources/webServer/src/utils/geocode.js
+++ completeNodejs/resources/webServer/src/utils/geocode.js
@@ -1,17 +1,18 @@
 const request = require('request')
 
-const geocode = (data) => {
+const geocode = data => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(data.address)}.json?access_token=pk.eyJ1IjoiZGFuZml0eiIsImEiOiJjazNkOGFnZzgwdWZrM2ZwYjdqejcxbmN2In0.Fz19l5uKH07WvZGSQXV3Ug&limit=1`
   
   return new Promise((resolve, reject) => {
     const config = { url, json: true }
 
-    request(config, (error, { body }) => {
+    request(config, (error, response) => {
       if (error) {
         reject('Unable to connect to geocode service')
-      } else if (body.features.length === 0) {
+      } else if (response.body.features.length === 0) {
         reject('Location input not found')
       } else {
+        const body = response.body
         const newData = {
           ...data,
           location: body.features[0].place_name,
