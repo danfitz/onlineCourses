38150e66b8ec5991436aad53d4d20a69d793b58f
Dan Fitz <dan@danfitz.com>
2021-03-22 09:21:04 -0400

    Moved course folders into language directories

---
+++ javascript/testingJavascript/resources/js-mocking-fundamentals/src/no-framework/mock-fn.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const thumbWar = require('../thumb-war');
+const utils = require('../utils');
+
+function fn(impl = () => {}) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  mockFn.mockImplementation = newImpl => (impl = newImpl);
+  return mockFn;
+}
+
+function spyOn(object, property) {
+  const originalImplementation = object[property];
+  object[property] = fn();
+  object[property].mockRestore = () => {
+    object[property] = originalImplementation;
+  };
+}
+
+spyOn(utils, 'getWinner');
+utils.getWinner.mockImplementation((p1, p2) => p1);
+
+const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+assert.strictEqual(winner, 'Kent C. Dodds');
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ['Kent C. Dodds', 'Ken Wheeler'],
+  ['Kent C. Dodds', 'Ken Wheeler'],
+]);
+
+// cleanup
+utils.getWinner.mockRestore();
